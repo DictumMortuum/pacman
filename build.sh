0460bedb8afb287a0a03d5e6770d6c2acddb87b4
DictumMortuum <theio.vrefos@gmail.com>
2022-09-28 22:22:30 +0300

    database-exporter v0.0.3-1[m

--- build.sh
+++ build.sh
@@ -1,20 +1,24 @@
 #!/bin/bash
 
-REPO=$1
-ARCH=$2
+ARCH=$1
 SERVER=satellite.dictummortuum.com
 
-if [[ -d $REPO ]]; then
-  cd $REPO
-  rm /tmp/repo/${REPO}*
-  sudo -u dimitris makepkg --config ../${ARCH}.conf -fc | tee /tmp/out
-  grep "Updated version" /tmp/out
-  MSG=$(grep -oP "Updated version: .*" /tmp/out | cut -d ' ' -f3- | sed 's/ / v/')
-  [[ -f /tmp/${REPO}.tar.gz ]] && rm /tmp/${REPO}.tar.gz
-  ARTIFACT=$(ls /tmp/repo/${REPO}*)
-  sudo -u dimitris scp $ARTIFACT ${SERVER}:/tmp
-  sudo -u dimitris ssh ${SERVER} "sudo cp /tmp/$(basename ${ARTIFACT}) /mnt/nfsserver/apps/repo/${ARCH}"
-  sudo -u dimitris ssh ${SERVER} "sudo /mnt/nfsserver/apps/repo/${ARCH}/create_repo.sh"
-  git cm "$MSG"
-  cd ..
-fi
+while (($#)); do
+  REPO=$2
+  shift
+  if [[ -d $REPO ]]; then
+    cd $REPO
+    rm /tmp/repo/${REPO}*
+    sudo -u dimitris makepkg --config ../${ARCH}.conf -fc | tee /tmp/out
+    grep "Updated version" /tmp/out
+    MSG=$(grep -oP "Updated version: .*" /tmp/out | cut -d ' ' -f3- | sed 's/ / v/')
+    [[ -f /tmp/${REPO}.tar.gz ]] && rm /tmp/${REPO}.tar.gz
+    ARTIFACT=$(ls /tmp/repo/${REPO}*)
+    sudo -u dimitris scp $ARTIFACT ${SERVER}:/tmp
+    sudo -u dimitris ssh ${SERVER} "sudo cp /tmp/$(basename ${ARTIFACT}) /mnt/nfsserver/apps/repo/${ARCH}"
+    git cm "$MSG"
+    cd ..
+  fi
+done
+
+sudo -u dimitris ssh ${SERVER} "sudo /mnt/nfsserver/apps/repo/${ARCH}/create_repo.sh"
